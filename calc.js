a81f4a2e19d2e8d6be9890bea4927880ec3ae044
Endand <endand2898@gmail.com>
2024-03-07 16:42:11 -0500

    Added operate function
    Now it takes two numbers and operator and perform action based on the operator

--- calc.js
+++ calc.js
@@ -11,4 +11,25 @@ function divide(a,b){
    return a/b;
 }
 
-console.log(divide(2,0));
+let firstNum=0;
+let secNum=0;
+let operator='';
+
+function operate(a, b, op) {
+   switch (op) {
+       case '+':
+           add(a, b);
+           break;
+       case '-':
+           subtract(a, b);
+           break;
+       case '*':
+           multiply(a, b);
+           break;
+       case '/':
+           divide(a, b);
+           break;
+       default:
+           console.error("Invalid operator");
+   }
+}
