2d4aa3bba9794ddf99fcf26ba3a99e60b1c78a8c
Endand <endand2898@gmail.com>
2024-03-07 19:07:43 -0500

    Added eventListner to buttons
    Now numbers, AC, Clear, +/- buttons have an effect on the display screen

--- calc.js
+++ calc.js
@@ -16,7 +16,7 @@ let secNum=0;
 let operator='';
 
 
-function operate(a, b, op) {
+function performOperation(a, b, op) {
    switch (op) {
        case '+':
            add(a, b);
@@ -32,9 +32,69 @@ function operate(a, b, op) {
            break;
        default:
            console.error("Invalid operator");
+           return NaN;
    }
 }
 
-function display(){
+const displayElement = document.querySelector('.display');
 
+function setupNumberListeners() {
+   const numbers = document.querySelectorAll('.number');
+   numbers.forEach(number => {
+      number.addEventListener('click', () => {
+         updateNumber(number.textContent);
+      });
+   });
 }
+
+function updateNumber(value) {
+   displayElement.textContent = displayElement.textContent === '0' 
+   ? value : displayElement.textContent + value;
+}
+
+function setupACListener() {
+   const ac = document.querySelector('.ac');
+   ac.addEventListener('click', () => {
+      clearDisplay();
+   });
+}
+
+function clearDisplay() {
+   displayElement.textContent = '0';
+}
+
+function setupClearListener() {
+   const clear = document.querySelector('.clear');
+   clear.addEventListener('click', () => {
+      clearOneDisplay();
+   });
+}
+
+function clearOneDisplay() {
+   content=displayElement.textContent;
+   content.length===2 && content[0]==='-' ?
+      clearDisplay() : displayElement.textContent = content.slice(0,content.length -1);
+}
+
+function setupSCListener() {
+   const sc = document.querySelector('.signChange');
+   sc.addEventListener('click', () => {
+      invertNumber();
+   });
+}
+
+function invertNumber() {
+   displayElement.textContent = String(Number(displayElement.textContent)*-1);
+}
+
+function initializeCalculator() {
+   setupNumberListeners();
+   setupACListener();
+   setupSCListener();
+   setupClearListener();
+}
+
+document.addEventListener('DOMContentLoaded', ()=>
+   initializeCalculator()
+);
+
